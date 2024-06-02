f9d719e2e83dbcc1c305bb227126c1ca287da50c
Felix Orwari <felix.orwari@gmail.com>
2024-06-03 01:59:12 +0300

    Creates exchanges page

---
+++ pages/exchanges.vue
@@ -0,0 +1,11 @@
+<script setup>
+const { data } = await useFetch("https://api.coinlore.net/api/exchanges/");
+</script>
+
+<template>
+  <div>
+    <h1 class="text-4xl font-bold leading-tight tracking-wide">
+      Top Cryptocurrency Exchanges
+    </h1>
+  </div>
+</template>
